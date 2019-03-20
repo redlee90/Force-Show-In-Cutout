278bdd88bdba50eb4a653a0cd7c78b272725418b
Rui Li <ruili@stanforhealthcare.org>
2019-03-20 15:42:48 -0700

    Removed unused code as EdXposed has support for white/black list; Removed launcher icon

--- app/src/main/java/com/lanyus/blocksecureflag/XposedMain.java
+++ app/src/main/java/com/lanyus/blocksecureflag/XposedMain.java
@@ -3,16 +3,8 @@ package com.lanyus.blocksecureflag;
 import android.os.Build;
 import android.view.WindowManager;
 
-import com.alibaba.fastjson.JSON;
-import com.lanyus.blocksecureflag.bean.CheckedApp;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XSharedPreferences;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -23,20 +15,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedMain implements IXposedHookLoadPackage {
 
-    private List<CheckedApp> checkedApps = new ArrayList<>();
-
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
-        XSharedPreferences preferences = new XSharedPreferences("com.lanyus.blocksecureflag", "checkedApp");
-        preferences.makeWorldReadable();
-        preferences.reload();
-        checkedApps = JSON.parseArray(preferences.getString("checkedApps", JSON.toJSONString(checkedApps)), CheckedApp.class);
-
-        if (lpparam.packageName.equals("com.lanyus.blocksecureflag") || !checkedApps.contains(new CheckedApp(lpparam.packageName))) {
-            return;
-        }
-        XposedBridge.log("com.lanyus.blocksecureflag Loaded app: " + lpparam.packageName);
 
         XposedHelpers.findAndHookMethod("android.view.Window", lpparam.classLoader, "setFlags", int.class, int.class, new XC_MethodHook() {
             @Override
